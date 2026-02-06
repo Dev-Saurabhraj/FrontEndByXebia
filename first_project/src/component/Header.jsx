4d8c380225b15c223083f23a1847dea22920a1d4
saurabh <saurabhraj2509@gmail.com>
2026-02-06 13:41:30 +0530

    final commit

--- first_project/src/component/Header.jsx
+++ first_project/src/component/Header.jsx
@@ -1,7 +1,14 @@
+ import './Header.css'
  export function Header(){
     return(
+        <>
         <h2>
             this my fist react app.
         </h2>
+        <nav className='navigation'>
+        <a href="#" > contact us </a>
+        </nav>
+        </>
+        
     )
 }
