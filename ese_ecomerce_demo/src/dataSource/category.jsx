20b5408926ee097fdb0c8827838ca21e4deb4b3f
saurabh <saurabhraj2509@gmail.com>
2026-05-11 18:57:37 +0530

    dummy data created as data source

---
+++ ese_ecomerce_demo/src/dataSource/category.jsx
@@ -0,0 +1,27 @@
+ const category = [
+    {
+      "id": 1,
+      "name": "Mobiles",
+      "icon": "smartphone"
+    },
+    {
+      "id": 2,
+      "name": "Laptops",
+      "icon": "laptop"
+    },
+    {
+      "id": 3,
+      "name": "Fashion",
+      "icon": "shirt"
+    },
+    {
+      "id": 4,
+      "name": "Electronics",
+      "icon": "headphones"
+    },
+    {
+      "id": 5,
+      "name": "Home Appliances",
+      "icon": "home"
+    }
+  ]
