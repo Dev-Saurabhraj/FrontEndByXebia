0a45b1993f15d477c87f8dfd6af0e411c9c9edeb
saurabh <saurabhraj2509@gmail.com>
2026-05-12 23:43:51 +0530

    change ui of card

--- ese_ecomerce_demo/src/components/mainSection.jsx
+++ ese_ecomerce_demo/src/components/mainSection.jsx
@@ -1,12 +1,12 @@
 export default function MainSection({items}){
     return (<>
-    <div className="grid grid-cols-5 gap-2">
+    <div className="grid grid-cols-4 gap-2 bg-gray-200">
     {items.map(item=>{
-        return (<div key={item.id} className="w-60 h-60 bg-sky-300 rounded-2xl ">
-            <h3>{item.title}</h3>
-        </div>)
+        return (<div key={item.id} className="w-60 h-60 bg-white rounded-2xl m-5 ml-5">
+            <h3 className="font-bold flex p-5 justify-center">{item.title}</h3>
+        </div>);
+        
     })}
-
     </div>
     </>)
 }
